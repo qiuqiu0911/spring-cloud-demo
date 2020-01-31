5de6067e47b4829c591282989fef9a3ce909d8d5
Shuangqiu Zheng <qiu_zs@126.com>
2020-01-31 16:58:29 +0800

    add demo eureka client

--- eureka-client/src/main/java/dev/qiuqiu/EurekaClientApplication.java
+++ eureka-client/src/main/java/dev/qiuqiu/EurekaClientApplication.java
@@ -2,12 +2,13 @@ package dev.qiuqiu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
-public class EurekaServer {
+public class EurekaClientApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServer.class, args);
+        SpringApplication.run(EurekaClientApplication.class, args);
     }
+
 }
+
