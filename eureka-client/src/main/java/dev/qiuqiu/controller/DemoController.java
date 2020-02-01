943c9a2d543c5190b853062e6ad95bdde019a517
Shuangqiu Zheng <qiu_zs@126.com>
2020-02-01 11:41:22 +0800

    add git commit id plugin

--- eureka-client/src/main/java/dev/qiuqiu/controller/DemoController.java
+++ eureka-client/src/main/java/dev/qiuqiu/controller/DemoController.java
@@ -1,13 +1,34 @@
 package dev.qiuqiu.controller;
 
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class DemoController {
+
+    public DemoController(DiscoveryClient discoveryClient) {
+        this.discoveryClient = discoveryClient;
+    }
 
     @GetMapping("/test")
     public String test() {
         return "test passed";
     }
+
+
+    private final DiscoveryClient discoveryClient;
+
+    @RequestMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(
+            @PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
+    }
+
+
 }
