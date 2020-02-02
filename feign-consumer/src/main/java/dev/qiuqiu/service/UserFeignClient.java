0a2ac4f2c09b6afba00f86291986bf098e0e1b11
Shuangqiu Zheng <qiu_zs@126.com>
2020-02-02 11:58:38 +0800

    add hystrix fallbackFactory

--- feign-consumer/src/main/java/dev/qiuqiu/service/UserFeignClient.java
+++ feign-consumer/src/main/java/dev/qiuqiu/service/UserFeignClient.java
@@ -1,23 +1,33 @@
 package dev.qiuqiu.service;
 
+import feign.hystrix.FallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 // 不支持下划线
-@FeignClient(name = "feign-user-service", fallback = HystrixFallBack.class)
+@FeignClient(name = "feign-user-service", fallbackFactory = HystrixFallBackFactory.class)
 public interface UserFeignClient {
 
     @GetMapping("/user/{userId}")
     String getUserCurrentInfo(@PathVariable("userId") String userId);
 }
 
+//@Component
+//class HystrixFallBack implements UserFeignClient{
+//
+//    @Override
+//    public String getUserCurrentInfo(String userId) {
+//        return "断路器打开";
+//    }
+//}
+
 @Component
-class HystrixFallBack implements UserFeignClient{
+class HystrixFallBackFactory implements FallbackFactory<UserFeignClient>{
 
     @Override
-    public String getUserCurrentInfo(String userId) {
-        return "断路器打开";
+    public UserFeignClient create(Throwable throwable) {
+        return userId -> "断路器打开:" + throwable.getMessage();
     }
 }
