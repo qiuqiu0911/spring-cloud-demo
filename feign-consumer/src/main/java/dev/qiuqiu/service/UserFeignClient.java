749aa3c1501ec5665bb22d780765d54acd6d7679
Shuangqiu Zheng <qiu_zs@126.com>
2020-02-02 12:02:12 +0800

    support @see https://cloud.spring.io/spring-cloud-static/spring-cloud-openfeign/2.2.1.RELEASE/reference/html/#feign-and-primary

--- feign-consumer/src/main/java/dev/qiuqiu/service/UserFeignClient.java
+++ feign-consumer/src/main/java/dev/qiuqiu/service/UserFeignClient.java
@@ -2,26 +2,28 @@ package dev.qiuqiu.service;
 
 import feign.hystrix.FallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 // 不支持下划线
-@FeignClient(name = "feign-user-service", fallbackFactory = HystrixFallBackFactory.class)
+@Primary
+@FeignClient(name = "feign-user-service", fallback = HystrixFallBack.class)
 public interface UserFeignClient {
 
     @GetMapping("/user/{userId}")
     String getUserCurrentInfo(@PathVariable("userId") String userId);
 }
 
-//@Component
-//class HystrixFallBack implements UserFeignClient{
-//
-//    @Override
-//    public String getUserCurrentInfo(String userId) {
-//        return "断路器打开";
-//    }
-//}
+@Component
+class HystrixFallBack implements UserFeignClient{
+
+    @Override
+    public String getUserCurrentInfo(String userId) {
+        return "断路器打开";
+    }
+}
 
 @Component
 class HystrixFallBackFactory implements FallbackFactory<UserFeignClient>{
